5c93c5e37adb4e80fe536370e75a3693cea096f7
NBAS-5535 <uras@yahoo.com>
2026-01-28 19:39:03 -0500

    pathplanner crash fixed; can't picj scenario, yet

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -9,6 +9,10 @@ import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -47,6 +51,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private final SwerveRequest.SysIdSwerveTranslation m_translationCharacterization = new SwerveRequest.SysIdSwerveTranslation();
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
+
+    /** Swerve request to apply during robot-centric path following */
+    private final SwerveRequest.ApplyRobotSpeeds m_pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
 
     /* follow AprilTag via Limelight*/
     public final SwerveRequest.RobotCentricFacingAngle visionFollowRequest = new SwerveRequest.RobotCentricFacingAngle();
@@ -143,7 +150,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             this.resetPose(m_initialPose);
         } else if ( DriverStation.getAlliance().get() == Alliance.Blue ){      
             this.resetPose(m_initialPose_opposite);
-        }   
+        } 
+        
+        // configure PathPlanner
+        configureAutoBuilder();
     }
 
     /**
@@ -171,6 +181,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
         /* Post the Field object to SmartDashboard once at startup */
         SmartDashboard.putData("Field", m_field);
+
+        configureAutoBuilder();
     }
 
     /**
@@ -375,6 +387,41 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         for ( int i = 0; i < 4; i++ ){
             this.getModule(i).getDriveMotor().stopMotor();
             this.getModule(i).getSteerMotor().stopMotor();
+        }
+    }
+
+    /**
+     * Configures PathPlanner.
+     *
+     * @param none
+     * @return void
+     */
+    private void configureAutoBuilder() {
+        try {
+            var config = RobotConfig.fromGUISettings();
+            AutoBuilder.configure(
+                () -> getState().Pose,   // Supplier of current robot pose
+                this::resetPose,         // Consumer for seeding pose against auto
+                () -> getState().Speeds, // Supplier of current robot speeds
+                // Consumer of ChassisSpeeds and feedforwards to drive the robot
+                (speeds, feedforwards) -> setControl(
+                    m_pathApplyRobotSpeeds.withSpeeds(speeds)
+                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
+                ),
+                new PPHolonomicDriveController(
+                    // PID constants for translation
+                    new PIDConstants(10, 0, 0),
+                    // PID constants for rotation
+                    new PIDConstants(7, 0, 0)
+                ),
+                config,
+                // Assume the path needs to be flipped for Red vs Blue, this is normally the case
+                () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
+                this // Subsystem for requirements
+            );
+        } catch (Exception ex) {
+            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
         }
     }
 }
